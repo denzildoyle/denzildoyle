53b166041e08f77b306e943215360b9d1e573dc5
Denzil Doyle <denzildoyle@gmail.com>
2024-04-09 01:31:04 -0400

    Remove unused js code

--- js/main.js
+++ js/main.js
@@ -1,48 +1,34 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // display greeting
+  var today = new Date();
+  var hour = today.getHours();
 
-	//get instagram photos from my account
-	var userFeed = new Instafeed({
-	    get: 'user',
-	    userId: 14494070,
-	    accessToken: '14494070.943f96e.dfd4a9148f9e412381530f8fde6dc29f',
-	    template: '<a href="{{link}}" class="col-3"><img src="{{image}}" /></a>',
-	    limit: 6,
-	    resolution: 'standard_resolution'
-	});
-	userFeed.run();
+  if (hour < 12) {
+    $("#greetings").html("morning");
+  } else if (hour >= 12 && hour <= 17) {
+    $("#greetings").html("afternoon");
+  } else if (hour === 18) {
+    $("#greetings").html("evening");
+  } else if (hour >= 19 && hour <= 24) {
+    $("#greetings").html("night");
+  } else {
+    $("#greetings").html("greetings");
+  }
 
-	// animation
-    // $(".bg-img").addClass("enlarged");
-    // $('#title').addClass('animated fadeInDown');
-    // $('#bio').addClass('animated fadeInUp');
-    // $('#line').addClass('animate');
+  // get copyright year
+  $("#year").html(today.getFullYear());
 
-    // display greeting
-    var today = new Date();
-	var hour = today.getHours();
-
-	if(hour < 12){
-		$('#greetings').html('morning');
-	} else if (hour >= 12 && hour <= 17){
-		$('#greetings').html('afternoon');
-	} else if (hour === 18){
-		$('#greetings').html('evening');
-	} else if (hour >= 19 && hour <= 24){
-		$('#greetings').html('night');
-	} else{
-		$('#greetings').html('greetings');
-	}
-
-	// get copyright year
-	$('#year').html(today.getFullYear());
-
-	// scroll to section
-    $('a[href^="#"]').on('click',function (e) {
-        e.preventDefault();
-        var target = this.hash,
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, 1000, 'swing');
-    });
+  // scroll to section
+  $('a[href^="#"]').on("click", function (e) {
+    e.preventDefault();
+    var target = this.hash,
+      $target = $(target);
+    $("html, body").stop().animate(
+      {
+        scrollTop: $target.offset().top,
+      },
+      1000,
+      "swing"
+    );
+  });
 });
